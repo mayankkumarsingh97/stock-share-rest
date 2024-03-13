a897f0cf57799acdd9e2390ce951543eaca35a7f
Animesh <animesh.kumar@escorts.co.in>
2024-03-13 17:43:15 +0530

    cert_mast ->search by certificate_nu,transfer_nu,Tag

--- controllers/certificateMast/certMastCtrl.js
+++ controllers/certificateMast/certMastCtrl.js
@@ -5,17 +5,50 @@ const certMastCtrl = (req, res) => {
     let security_type_desc = ''
     //
     const { folio_no, security_code } = req.body
+    const { certificate_no, tag } = req.body
+    const { dist_from, dist_to } = req.body
+    const { transfer_number, transfer_date } = req.body
+    const { seller_folio } = req.body
+
+    // console.log(folio_no, 'folio_no')
+    // console.log(security_code, 'security_code')
+    // console.log(certificate_no, 'certificate_no')
+    // console.log(tag, 'tag')
+    // console.log(dist_from, 'dist_from')
+    // console.log(dist_to, 'dist_to')
+    // console.log(transfer_number, 'transfer_number')
+    // console.log(transfer_date, 'transfer_date')
+    // console.log(seller_folio, 'seller_folio')
 
     try {
         connectToOracle().then((connection) => {
             //
-            const query = `SELECT no_of_sec,folio_no,certificate_number,distinctive_no_to,distinctive_no_from,transfer_number,transfer_date
-            FROM certificate_master WHERE rownum < = 300 AND folio_no=:folio_no and security_code=:security_code`
+            let query = `
+            SELECT no_of_sec,folio_no,certificate_number,distinctive_no_to,distinctive_no_from,transfer_number,transfer_date,CURR_STATUS,PREV_FOLIO_NUMBER
+            FROM certificate_master WHERE rownum < = 200 AND folio_no=:folio_no AND security_code=:security_code
+            `
+            //
+            const _qu_certificate_number_FIL = `AND certificate_number=${certificate_no}`
+            if (certificate_no) query += _qu_certificate_number_FIL
+            //
+            const _qu_tag_FIL = `AND CURR_STATUS='${tag}'`
+            if (tag) {
+                query += _qu_tag_FIL
+            }
+            //
+            const _qu_transfer_number_FIL = `AND transfer_number=${transfer_number}`
+            if (transfer_number) {
+                query += _qu_transfer_number_FIL
+            }
 
+            //
+            //-----------------------------------------------------------------------//
+            //-----------------------------------------------------------------------//
+            //
             connection.execute(query, [folio_no, security_code], (err, results) => {
 
                 if (err) {
-                    console.log("Err + ", err)
+                    console.log("Err : + ", err)
                     response = {
                         err: true,
                         message: 'fail fetch!',
@@ -23,7 +56,7 @@ const certMastCtrl = (req, res) => {
                     return res.status(500).send(JSON.stringify(response))
                 }
                 //
-                if (folio_no && security_code) {
+                if (security_code && folio_no) {
                     if (!err && results.rows.length > 0) {
                         var result_data = results.rows.map(function (row) {
                             return {
@@ -34,21 +67,24 @@ const certMastCtrl = (req, res) => {
                                 distinctive_no_from: row[4],
                                 transfer_number: row[5],
                                 transfer_date: row[6],
+                                holding: row[0],
                                 seller_folio: row[1],
+                                tag: row[7],
+                                seller_folio: row[8],
                             };
                         });
 
                     } else {
                         response = {
                             err: true,
-                            message: 'no data found',
+                            message: 'No data found / Please check if folio number or detail provided are correct !',
                         }
                         res.status(200).send(JSON.stringify(response))
                     }
                 } else {
                     response = {
                         err: true,
-                        message: 'folio_number or security_code missing',
+                        message: 'folio number or security_code missing',   
                     }
                     res.status(200).send(JSON.stringify(response))
                 }
@@ -63,7 +99,7 @@ const certMastCtrl = (req, res) => {
                             }
                         })
 
-                        const query2 = `SELECT *  FROM folio_master  WHERE folio_number =:folio_no`
+                        const query2 = `SELECT NAME_FIRST,NAME_LAST,NAME_MIDDLE,ADD1,ADD2,ADD3,PIN_ID,UNDLV_NO,UNDLV_DATE  FROM folio_master  WHERE folio_number =:folio_no`
                         //
                         connection.execute(query2, [folio_no], (err, results, next) => {
                             if (!err) {
@@ -76,7 +112,8 @@ const certMastCtrl = (req, res) => {
                                     err: false,
                                     security_type_desc,
                                     folio_det: result,
-                                    data: result_data, message: 'success!',
+                                    data: result_data, 
+                                    message: 'success!'
                                 }
                                 res.status(200).send(JSON.stringify(response))
 
@@ -104,6 +141,11 @@ const certMastCtrl = (req, res) => {
 
     } catch (err) {
         console.log('Err', + err)
+        response = {
+            err: true,
+            message: 'Internal server error / Please try again later',
+        }
+        res.status(500).send(JSON.stringify(response))
     }
 }
 
