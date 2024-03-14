69b973eabd851d50cc8937ffa0aabf41ba2940ee
Animesh <animesh.kumar@escorts.co.in>
2024-03-14 17:55:38 +0530

    new route etl-cert-mast added

--- routes/user.route.js
+++ routes/user.route.js
@@ -7,7 +7,8 @@ const { loginController } = require('../controllers/user/loginController')
 const { folioController } = require('../controllers/foliomaster/folioController')
 const { nameAddressQuery } = require('../controllers/foliomaster/nameAddHoldController')
 const { securityTypeCtrl } = require('../controllers/foliomaster/securityTypeCtrl')
-const { certMastCtrl} = require('../controllers/certificateMast/certMastCtrl')
+const { certMastCtrl } = require('../controllers/certificateMast/certMastCtrl')
+const { ETL_mast_ctrl } = require('../controllers/ETL_Ctrl/etl_mast_ctrl')
 //
 //---------------Login API----------------
 router.post('/escoshare/login/', loginController)
@@ -22,6 +23,8 @@ router.post('/folio/holding/', folioController)
 router.post('/name/address/holding/', nameAddressQuery)
 //
 router.post('/security/type/', securityTypeCtrl)
+//
+router.post('/etl-certificate-master', ETL_mast_ctrl)
 router.post('/certificate-master', certMastCtrl)
 
 //
