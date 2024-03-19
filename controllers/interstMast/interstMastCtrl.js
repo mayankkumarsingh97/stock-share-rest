0475d42f6b7d88322f9026a2f03516f64df39daa
Animesh <animesh.kumar@escorts.co.in>
2024-03-19 17:24:03 +0530

    newroute interst mast addes

---
+++ controllers/interstMast/interstMastCtrl.js
@@ -0,0 +1,154 @@
+const { query } = require('express');
+const connectToOracle = require('../../config/db')
+const oracledb = require('oracledb');
+
+//
+//
+const InterstMastCtrl = (req, res) => {
+    //
+    const { folio_no } = req.body
+    console.log(folio_no, 'folio no')
+    //
+    if (!folio_no) {
+        //
+        const response = {
+            err: true,
+            message: 'Err: folio_no missing'
+        }
+        return res.send(JSON.stringify(response))
+    }
+
+    try {
+        connectToOracle().then((connection) => {
+
+            let query = `
+            select  dir_p.description,  dm.*
+            from interest_masters dm 
+            Left Join dir_periods  dir_p on dm.date_of_payment = dir_p.date_of_payment 
+            where    folio_no=:folio_no
+            `;
+            //
+            //
+            let get_folio_details = `SELECT * FROM folio_master where folio_number=:folio_no`;
+
+            //
+            if (folio_no) {
+                //
+                connection.execute(`${query}`, [folio_no], (err, results) => {
+                    //
+                    if (!err) {
+                        if (results.rows.length > 0) {
+
+                            const result = results.rows.map(function (row) {
+                                return {
+                                    description: row[0],
+                                    date_of_payment: row[1],
+                                    security_code: row[2],
+                                    shares: row[3],
+                                    net_dividend_amount: row[4],
+                                    micr_no: row[5],
+                                    status_code: row[6],
+                                    folio_no: row[7],
+                                    voice_mail_status: row[8],
+                                };
+
+                            });
+
+                            let folio_det = {};
+                            //
+                            connection.execute(get_folio_details, [folio_no], (err, results) => {
+
+                                results.metaData.forEach((meta, index) => {
+                                    folio_det[meta.name] = results.rows[0][index];
+                                });
+
+                                const Ids = {
+                                    "DP_ID": "",
+                                    "CLIENT_ID": "",
+                                }
+                                //
+                                //
+                                response = {
+                                    err: false,
+                                    data: result,
+                                    clientIds: Ids,
+                                    folio_det,
+                                    message: 'success!'
+                                }
+                                //
+                                res.send(JSON.parse(JSON.stringify(response)))
+                            })
+
+
+
+                        } else {
+                            response = {
+                                err: true,
+                                message: 'check folio no/ no data found !'
+                            }
+                            res.send(JSON.stringify(response))
+                        }
+
+                    } else {
+                        response = {
+                            err: true,
+                            data: [],
+                            message: 'fail'
+                        }
+                        res.send(JSON.stringify(response))
+                    }
+                });
+            } else {
+                console.log('Err:', +err)
+                response = {
+                    err: true,
+                    data: [],
+                    message: 'fail'
+                }
+                res.send(JSON.stringify(response))
+            }
+
+        });
+
+    } catch (err) {
+        console.log('Err', + err)
+    }
+
+
+
+
+}
+
+module.exports = { InterstMastCtrl }
+
+
+
+
+
+
+
+
+
+
+
+
+// const connectToOracle = require("../../config/db")
+// const { connect } = require("../../routes/user.route")
+
+// const InterstMastCtrl = (req, res) => {
+
+//     const { folio_no } = req.body
+
+//     try {
+//         connectToOracle().then
+
+//     } catch (err) {
+//     }
+
+
+// }
+
+
+// module.exports = {
+//     InterstMastCtrl
+// }
